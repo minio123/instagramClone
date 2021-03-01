78384eed22e1bc14d205e71133b37315b790be7a
Herminio Amboni <minioamboni@gmail.com>
2021-03-01 20:53:09 +0800

    Change login logo into instagran logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,6 +20,11 @@
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
+          <!--   <div class="mt-4">
+                <x-jet-label for="username" value="{{ __('Username') }}" />
+                <x-jet-input id="username" class="block mt-1 w-full" type="username" name="username" :value="old('username')" required />
+            </div> -->
+
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
